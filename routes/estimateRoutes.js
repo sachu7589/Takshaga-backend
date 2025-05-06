59038289121bc7406b92e489610ac149eeecafe1
sachu <sachu7589@gmail.com>
2025-05-06 09:52:40 +0530

    estimate issue fixed 2

--- routes/estimateRoutes.js
+++ routes/estimateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Estimate = require('../models/estimate.js');
+const Estimate = require('../models/Estimate');
 const Client = require('../models/client');
 
 // Create a new estimate
